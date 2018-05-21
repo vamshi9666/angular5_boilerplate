f70ec99ab618dce79237fd262dc1dbe009517073
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-05-21 15:46:20 +0530

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,7 @@ import { EmailValidator } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+	
+}
 
